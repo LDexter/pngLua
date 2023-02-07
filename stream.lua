8af32b5d7badabf353f0e685b863292964355c43
9551Dev <oliverekcaha@gmail.com>
2023-02-07 22:42:56 +0100

    Holy shit its done

--- stream.lua
+++ stream.lua
@@ -1,39 +1,27 @@
-class = require '30log'
-Stream = class()
-Stream.data = {}
-Stream.position = 1
-Stream.__name = "Stream"
+local Stream = {}
 
-function Stream:bsRight(num, pow)
-    return math.floor(num / 2^pow)
-end
+setmetatable(Stream,{__call=function(methods,param)
+	local self = {
+		position = 1,
+		data     = {}
+	}
 
-function Stream:bsLeft(num, pow)
-    return math.floor(num * 2^pow)
-end
-
-function Stream:bytesToNum(bytes)
-	local n = 0
-	for k,v in ipairs(bytes) do
-		n = self:bsLeft(n, 8) + v
-	end
-	n = (n > 2147483647) and (n - 4294967296) or n
-	return n
-end
-
-function Stream:__init(param)
     local str = ""	
     if (param.inputF ~= nil) then
-	str = io.open(param.inputF, "rb"):read("*all")
+		local file = fs.open(param.inputF, "rb")
+		str = file.readAll()
+		file.close()
     end
     if (param.input ~= nil) then
-	str = param.input
+		str = param.input
     end
 
     for i=1,#str do
-	self.data[i] = str:byte(i, i)
+		self.data[i] = str:byte(i, i)
     end
-end
+
+	return setmetatable(self,{__index=methods})
+end})
 
 function Stream:seek(amount)
 	self.position = self.position + amount
@@ -44,6 +32,11 @@ function Stream:readByte()
 	local byte = self.data[self.position]
 	self:seek(1)
 	return byte
+end
+
+function Stream:readChar()
+	if self.position <= 0 then self:seek(1) return nil end
+	return string.char(self:readByte())
 end
 
 function Stream:readChars(num)
@@ -57,47 +50,42 @@ function Stream:readChars(num)
 	return str, i-1
 end
 
-function Stream:readChar()
-	if self.position <= 0 then self:seek(1) return nil end
-	return string.char(self:readByte())
-end
-
-function Stream:readBytes(num)
-	if self.position <= 0 then self:seek(1) return nil end
-	local tabl = {}
-	local i = 1
-	while i <= num do
-		local curByte = self:readByte()
-		if curByte == nil then break end
-		tabl[i] = curByte
-		i = i + 1
-	end
-	return tabl, i-1
-end
-
 function Stream:readInt(num)
 	if self.position <= 0 then self:seek(1) return nil end
+
 	num = num or 4
+
 	local bytes, count = self:readBytes(num)
+
 	return self:bytesToNum(bytes), count
 end
 
-function Stream:writeByte(byte)
-	if self.position <= 0 then self:seek(1) return end
-	self.data[self.position] = byte
-	self:seek(1)
+function Stream:readBytes(num)
+    if self.position <= 0 then self:seek(1) return nil end
+
+    local tabl = {}
+    local i = 1
+
+    while i <= num do
+        local curByte = self:readByte()
+        if curByte == nil then break end
+
+        tabl[i] = curByte
+        i = i + 1
+    end
+    return tabl, i-1
 end
 
-function Stream:writeChar(char)
-	if self.position <= 0 then self:seek(1) return end
-	self:writeByte(string.byte(char))
-end
+function Stream:bytesToNum(bytes)
+	local n = 0
 
-function Stream:writeBytes(buffer)
-	if self.position <= 0 then self:seek(1) return end
-	local str = ""
-	for k,v in pairs(buffer) do
-		str = str .. string.char(v)
+	for _,v in ipairs(bytes) do
+		n = n*256 + v
 	end
-	writeChars(str)
+
+	n = (n > 2147483647) and (n - 4294967296) or n
+
+	return n
 end
+
+return Stream
