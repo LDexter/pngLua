0856f220c6a39a60cb54d17035067e54c9c6eade
9551Dev <oliverekcaha@gmail.com>
2023-02-07 23:28:35 +0100

    oh dear lord why the tabs

--- test.lua
+++ test.lua
@@ -5,34 +5,34 @@ local image = png_lua("pngLua/Example.png")
 
 local palette = {}
 for i=0,15 do
-	palette[2^i] = {term.getPaletteColor(2^i)}
+    palette[2^i] = {term.getPaletteColor(2^i)}
 end
 
 local function find_closest_color(c)
     local n,result = 0,{}
 
-	for k,v in pairs(palette) do
-		n=n+1
-		result[n] = {
-			dist=math.sqrt(
-				(v[1]-c.r)^2 +
-				(v[2]-c.g)^2 +
-				(v[3]-c.b)^2
-			),  color=k,
-			r=v[1],g=v[2],b=v[3]
-		}
-	end
+    for k,v in pairs(palette) do
+        n=n+1
+        result[n] = {
+            dist=math.sqrt(
+                (v[1]-c.r)^2 +
+                (v[2]-c.g)^2 +
+                (v[3]-c.b)^2
+            ),  color=k,
+            r=v[1],g=v[2],b=v[3]
+        }
+    end
 
-	table.sort(result,function(a,b) return a.dist < b.dist end)
+    table.sort(result,function(a,b) return a.dist < b.dist end)
 
     local r = result[1]
     return r.color,r.r,r.g,r.b
 end
 
 for x=1,image.width do
-	for y=1,image.height do
-		pixelbox.CANVAS[y][x] = find_closest_color(image:get_pixel(x,y))
-	end
+    for y=1,image.height do
+        pixelbox.CANVAS[y][x] = find_closest_color(image:get_pixel(x,y))
+    end
 end
 
 pixelbox:render()
